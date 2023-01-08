8260a168f019844b087cd8257e946d5aeb8b98bf
Анна Литвинова <anna132407@gmail.com>
2023-01-09 00:51:49 +0200

    1-2-3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+const imagesList = document.querySelector('.gallery');
+
+function createImagesMarkup ({preview, original, description}){
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}
+
+const gallery = galleryItems.map(createImagesMarkup).join('');
+imagesList.innerHTML = gallery;
+
+let showGallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,55 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    message: document.querySelector('.feedback-form textarea'),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.message.addEventListener('input', throttle(onMessageInput, 500));
+
+refs.form.addEventListener('input', event => {
+    formData[event.target.email] = event.target.value;
+    console.log(formData);
+});
+
+populateMessage();
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    if (event.target.email.value.trim() === '' || event.target.message.value.trim() === '') {
+        alert('Please fill all fields');
+        return;
+    }
+
+    console.log({email: refs.email.value, message: refs.message.value});
+    
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function onMessageInput(event) {
+    const message = event.target.value;
+    localStorage.setItem(STORAGE_KEY, message);
+}
+
+
+function onFormInput() {
+    const formData = JSON.stringify({email: refs.email.value, message: refs.message.value})
+    localStorage.setItem(STORAGE_KEY, formData);
+}
+
+
+function populateMessage() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    refs.message.value = savedMessage;
+  }
+}
 
